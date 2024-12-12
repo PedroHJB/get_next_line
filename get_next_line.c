d94a28096c154d55339d43da779520d532f8b46e
Pedro Henrique De Jesus Barbosa <pede-jes@c2r2p1.42sp.org.br>
2024-12-12 18:24:27 -0300

    ajustado

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,20 @@
 /*   By: pede-jes <pede-jes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 11:38:13 by pede-jes          #+#    #+#             */
-/*   Updated: 2024/12/12 15:39:37 by pede-jes         ###   ########.fr       */
+/*   Updated: 2024/12/12 18:22:44 by pede-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	*free_static(char **first_str, char **second_str)
+{
+	if (first_str &&*first_str)
+		free(*first_str);
+	if (second_str &&*second_str)
+		free(*second_str);
+	return (NULL);
+}
 
 char	*get_next_line(int fd)
 {
@@ -24,12 +33,13 @@ char	*get_next_line(int fd)
 			return (NULL);
 		string[0] = '\0';
 	}
-	if (BUFFER_SIZE < 0 || fd < 0)
+	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
 	string = ft_allocate(string, fd);
+	if (!string)
+		return (free_static(&string, NULL));
 	out = ft_next_line(string);
 	string = ft_remove_line(string);
-	// printf("%s",out);
 	return (out);
 }
 
@@ -37,21 +47,24 @@ char	*ft_allocate(char *string, int fd)
 {
 	int		count;
 	char	*aux_buffer;
+	char	*temp;
 
 	count = 1;
 	aux_buffer = malloc(BUFFER_SIZE + 1);
 	if (!aux_buffer)
-		return (NULL);
+		return (free_static(&string, NULL));
 	while ((!ft_strchar(string, '\n') && count > 0))
 	{
 		count = read(fd, aux_buffer, BUFFER_SIZE);
 		if (count < 0)
-		{
-			free(aux_buffer);
-			return (NULL);
-		}
+			return (free_static(&aux_buffer, &string));
 		aux_buffer[count] = '\0';
-		string = ft_str_join(string, aux_buffer);
+		temp = ft_str_join(string, aux_buffer);
+		if (!temp) // Garantia contra falha na alocação
+			return (free_static(&aux_buffer, &string));
+		string = temp;
+		if (count == 0 && string[0] == '\0') // Arquivo vazio
+			return (free_static(&aux_buffer, &string));
 	}
 	free(aux_buffer);
 	return (string);
@@ -77,24 +90,20 @@ char	*ft_str_join(char *string, char *string_to_join)
 	int		i;
 	int		j;
 
-	// if (!string)
-	// {
-	// 	string = malloc(1);
-	// 	if(!string)
-	// 		return(NULL);
-	// 	string[0] = '\0';
-	// }
 	i = 0;
 	j = 0;
 	string_joined = malloc(ft_str_len(string) + ft_str_len(string_to_join) + 1);
 	if (!string_joined)
+	{
+		free(string);
 		return (NULL);
+	}
 	while (string[j])
 		string_joined[i++] = string[j++];
-	i = 0;
-	while (string_to_join[i])
-		string_joined[j++] = string_to_join[i++];
-	string_joined[j] = '\0';
+	j = 0;
+	while (string_to_join[j])
+		string_joined[i++] = string_to_join[j++];
+	string_joined[i] = '\0';
 	free(string);
 	return (string_joined);
 }
@@ -118,8 +127,8 @@ char	*ft_next_line(char *string)
 	while (string[i] && string[i] != '\n')
 		i++;
 	new_string = malloc(i + 2);
-	 if (new_string== NULL)
-	 	return (NULL);
+	if (!new_string)
+		return (NULL);
 	i = 0;
 	while (string[i] && string[i] != '\n')
 	{
@@ -145,12 +154,18 @@ char	*ft_remove_line(char *string)
 	j = 0;
 	while (string[i] && string[i] != '\n')
 		i++;
-	new_string = malloc((ft_str_len(string) - i) + 2);
-	if(!new_string)
+	if (!string[i])
+	{
+		free(string);
 		return (NULL);
-	i++;
+	}
+	new_string = malloc((ft_str_len(string) - i) + 1);
 	if (!new_string)
+	{
+		free(string);
 		return (NULL);
+	}
+	i++;
 	while (string[i])
 		new_string[j++] = string[i++];
 	new_string[j] = '\0';
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: pede-jes <pede-jes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 11:39:01 by pede-jes          #+#    #+#             */
-/*   Updated: 2024/12/12 13:28:38 by pede-jes         ###   ########.fr       */
+/*   Updated: 2024/12/12 17:39:44 by pede-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 #  define BUFFER_SIZE 10
 # endif
 
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
 char	*get_next_line(int fd);
 char	*ft_allocate(char *string, int fd);
 char	*ft_strchar(char *string, int c);
@@ -24,11 +30,5 @@ char	*ft_str_join(char *string, char *string_to_join);
 int		ft_str_len(char *string);
 char	*ft_next_line(char *string);
 char	*ft_remove_line(char *string);
-
-# include <fcntl.h>
-# include <stddef.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
 
 #endif
